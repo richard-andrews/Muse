441a32aea7db81c595e0898ef090907965fe1529
Richard Andrews <richardxndrews@gmail.com>
2023-03-27 16:27:12 +0100

    Experiment changing iFrame source

---
+++ muse/scripts/change_artefact.js
@@ -0,0 +1,3 @@
+function select_random_artefact(){
+    "/embed?autostart=1&camera=0&annotations_visible=1&transparent=1&ui_animations=0&ui_inspector=0&ui_ar=1&ui_settings=0&ui_vr=0&ui_fullscreen=0&dnt=1"
+}
