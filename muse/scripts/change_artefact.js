abfb2c4de3a38eec22d337f28166f191a273dbc3
Richard Andrews <richardxndrews@gmail.com>
2023-03-27 16:55:25 +0100

    Added button to change artefact

--- muse/scripts/change_artefact.js
+++ muse/scripts/change_artefact.js
@@ -1,3 +1,11 @@
-function select_random_artefact(){
-    "/embed?autostart=1&camera=0&annotations_visible=1&transparent=1&ui_animations=0&ui_inspector=0&ui_ar=1&ui_settings=0&ui_vr=0&ui_fullscreen=0&dnt=1"
+const before = "https://sketchfab.com/models/";
+var id = "752e69d34933438d8230ac829d22300e";
+const after = "/embed?autostart=1&camera=0&annotations_visible=1&transparent=1&ui_animations=0&ui_inspector=0&ui_ar=1&ui_settings=0&ui_vr=0&ui_fullscreen=0&dnt=1";
+
+document.getElementById("myBtn").onclick = displayRandomArtefact;
+
+function displayRandomArtefact(){
+    var link = before.concat(id, after);
+    document.getElementById("iframe").src = link;
+    
 }
