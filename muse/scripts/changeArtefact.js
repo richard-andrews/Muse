cb33736f9d93e1dfc12d71e0928c559ef96592c9
Richard Andrews <richardxndrews@gmail.com>
2023-04-01 15:21:39 +0100

    Rename file and disable annotations on model

--- muse/scripts/changeArtefact.js
+++ muse/scripts/changeArtefact.js
@@ -3,12 +3,13 @@ const artefactIDs = ["752e69d34933438d8230ac829d22300e",
                      "64d0b7662b59417986e9d693624de97a",
                      "4a3b1d4de81146acba80c95a55d369ed",
                      "0001e919977447cda727b1688a697d71"];
-const after = "/embed?autostart=1&camera=0&annotations_visible=1&transparent=1&ui_animations=0&ui_inspector=0&ui_ar=1&ui_settings=0&ui_vr=0&ui_fullscreen=0&dnt=1";
+const after = "/embed?ui_annotations=0&autostart=1&camera=0&annotations_visible=1&transparent=1&ui_animations=0&ui_inspector=0&ui_ar=1&ui_settings=0&ui_vr=0&ui_fullscreen=0&dnt=1";
 
 var position = 0;
 
-document.getElementById("left").onclick = previousArtefact;
-document.getElementById("right").onclick = nextArtefact;
+document.getElementById("leftButton").onclick = previousArtefact;
+document.getElementById("leftButtonMobile").onclick = previousArtefact;
+document.getElementById("rightButton").onclick = nextArtefact;
 
 function previousArtefact(){
     position = loopPrev(position);
