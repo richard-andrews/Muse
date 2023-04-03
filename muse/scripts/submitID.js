1e8367b250a919bf6f69a3b15e4f3e1e4f12322f
Richard Andrews <richardxndrews@gmail.com>
2023-04-03 17:05:00 +0100

    ID input box bounces if no ID entered

--- muse/scripts/submitID.js
+++ muse/scripts/submitID.js
@@ -1,18 +1,31 @@
 document.getElementById("submitButton").onclick = presentMuse;
 
+
 function presentMuse(){
     pID = document.getElementById("idInput").value;
 
     if (pID == ""){
         document.getElementById("listenButton").classList.add("disabled");
         document.getElementById("interactButton").classList.add("disabled");
+        document.getElementById("idInput").style.borderColor = "#D499B9";
+        document.getElementsByTagName("h2")[0].style.color = "#D499B9";
         alert("Please enter your ID");
+        
+        setTimeout(resetColors, 2000);
     }
     else if (pID % 2 == 0){
-        document.getElementById("listenButton").classList.remove("disabled");
-        document.getElementById("interactButton").classList.add("disabled");
+        enableDisable("listenButton", "interactButton");
     } else if (pID % 2 == 1){
-        document.getElementById("listenButton").classList.add("disabled");
-        document.getElementById("interactButton").classList.remove("disabled");
+        enableDisable("interactButton", "listenButton");
     }
+}
+
+function enableDisable(enable, disable){
+    document.getElementById(enable).classList.add("disabled");
+    document.getElementById(disable).classList.remove("disabled");
+}
+
+function resetColors(){
+    document.getElementById("idInput").style.borderColor = "#FBF3D6";
+    document.getElementsByTagName("h2")[0].style.color = "#FBF3D6";
 }
